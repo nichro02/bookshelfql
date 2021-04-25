c95280d3400f769245d5bc5ae4e9b168da65cc1f
nichro02 <nichro02@gmail.com>
2021-04-25 14:57:02 -0600

    Refactored Book id field to be a GraphDQLID field so that either a string or an integer can be passed in for id

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,7 +7,7 @@ const graphql = require('graphql')
 //install lodash to help query database
 const _=require('lodash')
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql
 
 // dummy data
 var books = [
@@ -19,7 +19,7 @@ var books = [
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
     })
@@ -31,7 +31,7 @@ const RootQuery = new GraphQLObjectType({
         //query for particular book
         book: {
             type: BookType,
-            args: {id: {type: GraphQLString}},
+            args: {id: {type: GraphQLID}},
             resolve(parent, args){
                 //code to get data from database
                 return _.find(books, {id: args.id})
