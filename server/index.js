fa1b87b38a1df5ded519ca190e2737e5f5fd3be0
nichro02 <nichro02@gmail.com>
2021-05-01 14:23:18 -0600

    Set up connection to MongoDB

--- server/index.js
+++ server/index.js
@@ -2,8 +2,19 @@ const express = require('express')
 //set up middleware: https://www.npmjs.com/package/express-graphql
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express()
+require('dotenv').config()
+
+//connect to MongoDB database
+mongoose.connect(`${process.env.MONGODB_URI}`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+mongoose.connection.once('open', () => {
+    console.log('connected to database')
+})
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
