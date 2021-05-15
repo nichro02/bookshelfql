4396003fd8a2ec8329216ff25b3710fcafff0d89
nichro02 <nichro02@gmail.com>
2021-05-15 14:30:19 -0600

    Removed unnecessary files, created BookList component

---
+++ client/src/components/BookList.js
@@ -0,0 +1,11 @@
+function BookList() {
+    return (
+      <div>
+        <ul id='book-list'>
+            <li>Book name placeholder</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default BookList;
