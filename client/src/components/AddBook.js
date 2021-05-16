f1f524f621670b8180a1a44c83308659f81169ff
nichro02 <nichro02@gmail.com>
2021-05-16 16:24:50 -0600

    Added state to AddBook component to capture form information so it can be sent to database

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import {getAllAuthorsQuery} from '../queries/queries'
 
@@ -16,29 +17,82 @@ function GetAuthors(){
 }
 
 function AddBook(){
+    const [title, setTitle] = useState('')
+    const [genre, setGenre] = useState('')
+    const [plot, setPlot] = useState('')
+    const [authorId, setAuthorId] = useState('')
+
+    const data = {
+        title: title,
+        genre: genre,
+        plot: plot,
+        authorId: authorId
+    }
+
+    //update title
+    const updateTitle = e => {
+        setTitle(e.target.value)
+        console.log('TITLE',e.target.value)
+    }
+    //update genre
+    const updateGenre = e => {
+        setGenre(e.target.value)
+        console.log('GENRE',e.target.value)
+    }
+    //update genre
+    const updatePlot = e => {
+        setPlot(e.target.value)
+        console.log('PLOT',e.target.value)
+    }
+    //update author
+    const updateAuthorId = e => {
+        setAuthorId(e.target.value)
+        console.log('AUTHOR',e.target.value)
+    }
+    
+    //send book info to database
+    const handleBook = e => {
+        console.log(data)
+    }
+    
     return(
-        <form id='addBook'>
+        <form id='addBook' onSubmit={handleBook}>
             <div className='field'>
-                <label>Book Name:</label>
-                <input type='text' />
+                <label>Book Title:</label>
+                <input 
+                    type='text'
+                    value={title}
+                    onChange={updateTitle}
+                />
             </div>
             <div className='field'>
                 <label>Genre:</label>
-                <input type='text' />
+                <input
+                    type='text'
+                    value={genre}
+                    onChange={updateGenre}
+                />
             </div>
             <div className='field'>
                 <label>Plot:</label>
-                <input type='text' />
+                <input
+                    type='text'
+                    value={plot}
+                    onChange={updatePlot}
+                />
             </div>
             <div className='field'>
                 <label>Author:</label>
-                <select>
+                <select
+                    value={authorId}
+                    onChange={updateAuthorId}
+                >
                     <option>Select author</option>
                     {GetAuthors()}
                 </select>
             </div>
 
-            <button>+</button>
+            <button type='submit'>+</button>
 
         </form> 
     )
